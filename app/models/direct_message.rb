a6e681af4e330c5a82b62cf2649b2ad9db164140
umekichi777 <umekichi7teru@gmail.com>
2022-05-30 15:31:49 +0000

    [Add] Broadcast

--- app/models/direct_message.rb
+++ app/models/direct_message.rb
@@ -1,2 +1,8 @@
 class DirectMessage < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :room
+
+  after_create_commit { DirectMessageBroadcastJob.perform_later self }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  has_many :entries
+  has_many :direct_messages
+  has_many :rooms, through: :entries
+
 end
